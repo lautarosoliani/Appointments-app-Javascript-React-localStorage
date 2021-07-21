692ef0637d9d977523d600f1137f2faa70412d02
Lautaro <lautarosoliani@hotmail.com>
2021-07-21 19:36:50 -0300

    Commit 3 - 54

--- myreactapp/src/App.js
+++ myreactapp/src/App.js
@@ -2,11 +2,12 @@ import React, { Fragment } from 'react';
 import Formulario from './components/Formulario';
 
 //FRAMEWORK --> SKELETON
+//Second Commit corresponde al video 53 de Udemy
 
 function App() {
   return (
     <Fragment>
-      <h1>Administrador de pacientes</h1>
+      <h1>Turno Veterinario</h1>
 
       <div className="container">
         <div className="row">
