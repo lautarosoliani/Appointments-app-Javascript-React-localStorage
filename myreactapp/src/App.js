4eee4320e7193f331984d8c4e3fd2bf581fdc476
Lautaro <lautarosoliani@hotmail.com>
2021-07-22 13:52:40 -0300

    62

--- myreactapp/src/App.js
+++ myreactapp/src/App.js
@@ -15,6 +15,12 @@ function App() {
     guardarCitas([...citas, cita])
   }
 
+  //Funcion que elimina citas por su ID
+  const eliminarCita = id => {
+    const nuevasCitas = citas.filter(cita => cita.id !== id);
+    guardarCitas(nuevasCitas);
+  }
+
 
   return (
     <Fragment>
@@ -33,6 +39,7 @@ function App() {
               <Cita
                 key={cita.id}
                 cita={cita}
+                eliminarCita={eliminarCita}
               />
             ))}
           </div>
